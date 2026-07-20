9a0612a3ddbe8ba5e8b110ee808a1f0adcc5026b
SusyGlez <susanaglezb@gmail.com>
2026-07-20 15:09:24 +0200

    added media query

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const getSchemeBtn = document.getElementById("get-scheme-btn");
 let colorPicker = document.getElementById("color-picker");
 
 function giveColorScheme(data) {
-  let html = "";
-  for (let color of data.colors) {
-    html += `<div class="c-1" id="color-1" style="background:${color.hex.value}"></div>`;
-  }
-  document.getElementById("color-scheme").innerHTML = html;
+  let schemehtml = "";
+  let hexhtml = "";
+  data.colors.forEach((color, index) => {
+    schemehtml += `<div class="c-1" id="color-1" style="background:${color.hex.value}"></div>`;
+
+    hexhtml += `<div class="hex-item" id="hex-${index}">
+    <span class="hex-value">${color.hex.value}</span>
+    <button class="copy-btn" data-hex="${color.hex.value}"><img class="copy-btn-img"
+              src="./images/copy-img.png"
+            /></button>
+    </div>`;
+  });
+  document.getElementById("color-scheme").innerHTML = schemehtml;
+  document.getElementById("hex-values").innerHTML = hexhtml;
 }
+
+document.getElementById("hex-values").addEventListener("click", (event) => {
+  const copyBtn = event.target.closest(".copy-btn");
+  if (copyBtn) {
+    const hexValue = copyBtn.dataset.hex;
+    navigator.clipboard.writeText(hexValue);
+    alert("Copied the hex code: " + hexValue);
+  }
+});
 
 function fetchColorScheme() {
   const hex = colorPicker.value.replace("#", "");
